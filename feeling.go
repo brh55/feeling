70bbe293f49268ef3a994fb4a009cef448f049b1
Brandon Him <brh55@drexel.edu>
2016-01-29 20:00:53 -0500

    Fix the syntax

--- feeling.go
+++ feeling.go
@@ -1,48 +1,46 @@
 // Package feeling provides terminal output of feelings through text emoticons.
 package feeling
 
-import (
-    "github.com/fatih/color"
-)
+import "github.com/fatih/color"
 
 // Uncertain appends (,,◕　⋏　◕,,) in blue
-func Uncertain(text string) (string) {
+func Uncertain(text string) string {
     text += color.BlueString(" (,,◕　⋏　◕,,)")
     return text
 }
 
 // Whatever appends ¯\\_(ツ)_/¯ in green
-func Whatever(text string) (string) {
-     text += color.GreenString("¯\\_(ツ)_/¯")
+func Whatever(text string) string {
+     text += color.GreenString(" ¯\\_(ツ)_/¯")
      return text
 }
 
 // Scared appends ヽ(ﾟДﾟ)ﾉ in yellow
-func Scared(text string) (string) {
+func Scared(text string) string {
     text += color.YellowString(" ヽ(ﾟДﾟ)ﾉ")
     return text
 }
 
 // Upset appends (╯°□°）╯︵ ┻━┻ in cyan
-func Upset(text string) (string) {
+func Upset(text string) string {
     text += color.CyanString(" (╯°□°）╯︵ ┻━┻")
-    return text;
+    return text
 }
 
 // Cheerful appends (๑´▿｀๑)♫•*¨*•.¸¸♪✧ in cyan
-func Cheerful(text string) (string) {
+func Cheerful(text string) string {
     text += color.CyanString(" (๑´▿｀๑)♫•*¨*•.¸¸♪✧")
-    return text;
+    return text
 }
 
 // Loved appends (*＾3＾）/～♡ in red
-func Loved(text string) (string) {
+func Loved(text string) string {
     text += color.RedString(" (*＾3＾）/～♡")
-    return text;
+    return text
 }
 
 // Stumped appends (；¬＿¬) in blue
 func Stumped(text string) (string) {
-    text += color.BlueString("(；¬＿¬)")
-    return text;
+    text += color.BlueString(" (；¬＿¬)")
+    return text
 }
